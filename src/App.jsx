88b8eae22d21a0a88ec156b1c38866ab3ec5c66e
linabang <linakalneva@gmail.com>
2024-11-02 15:32:26 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  let name = 'Lina';
+  return (
+    <div>
+      <h1>Hallo world</h1>
+      <h2>Hallo {name}</h2>
+    </div>
+  )
+}
+
+export default App
