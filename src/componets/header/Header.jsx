a75f075f0c5661869a1273669eb148f197132624
linabang <linakalneva@gmail.com>
2024-11-04 22:05:57 +0600

    chenge index.jsx

--- src/componets/header/Header.jsx
+++ src/componets/header/Header.jsx
@@ -1,8 +1,23 @@
 import './Header.css'
+import logo from './assets/icons8-github 2.svg'
 const Header = () => {
   return (
-    <header className='header'>
-      <h1 className="hesder__title">Hallo</h1>
+    
+    <header className="header">
+      <div className="container header__wrapper">
+        <div className="header__logo">
+          <img src={logo} alt="логотип" />
+        </div>
+        <nav className="header__nav">
+          <ol className="header__nav__item">
+            <li className="header__nav__link">Главная</li>
+            <li className="header__nav__link">О нас</li>
+            <li className="header__nav__link">Корзина</li>
+            <li className="header__nav__link">Магазин</li>
+          </ol>
+        </nav>
+        <button className="header__btn">Войти</button>
+      </div>
     </header>
   )
 }
